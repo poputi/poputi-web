e8b19330d8486c2451a472d850e2049f8e053842
Matvey Kupriyanov <motya202@gmail.com>
2021-03-18 19:40:34 +0500

    убрать из гита launchSettings.json

---
+++ Poputi.DataAccess/Contexts/MainContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poputi.DataAccess.Daos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poputi.DataAccess.Contexts
+{
+    public class MainContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<CityRoute> CityRoutes { get; set; }
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Poputi.Web/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poputi.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string Login { get; set; }
+        public ICollection<CityRoute> HostedRoutes { get; set; }
+    }
+}
