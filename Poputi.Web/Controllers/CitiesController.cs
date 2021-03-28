9b11f580659d92a7dbe1cc693845ddbeb33a93c7
Matvey Kupriyanov <motya202@gmail.com>
2021-03-28 20:35:56 +0500

    добавил конвертер для пространственных данных и сделал контроллер для городов без слоя с сервисами

---
+++ Poputi.Web/Controllers/CitiesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Poputi.DataAccess.Daos;
+using Poputi.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Poputi.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly IGenericRepository _repository;
+
+        public CitiesController(IGenericRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async ValueTask<ActionResult<List<City>>> GetCities(CancellationToken cancellationToken)
+        {
+            return await _repository.Read<City>().ToListAsync(cancellationToken);
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async ValueTask<ActionResult<City>> GetCity(int id, CancellationToken cancellationToken)
+        {
+            City city = await _repository.Read<City>(new object[] { id }, token: cancellationToken);
+            if (city is null)
+            {
+                return NotFound();
+            }
+            return city;
+        }
+
+        //// PUT: api/Cities/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(int id, City city, CancellationToken cancellationToken)
+        {
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+
+            _repository.Update<City>(city);
+
+            try
+            {
+                await _repository.SaveChangesAsync(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //// POST: api/CityRoutes
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async ValueTask<ActionResult<City>> PostCity(City city, CancellationToken cancellationToken)
+        {
+            await _repository.Create(city, cancellationToken);
+            await _repository.SaveChangesAsync(cancellationToken);
+            return CreatedAtAction("GetCity", new { id = city.Id }, city);
+        }
+
+        //// DELETE: api/CityRoutes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(int id, CancellationToken cancellationToken)
+        {
+            var city = await _repository.Read<City>(new object[] { id }, cancellationToken);
+            if (city is null)
+            {
+                return NotFound();
+            }
+
+            _repository.Remove(city);
+            await _repository.SaveChangesAsync(cancellationToken);
+
+            return NoContent();
+        }
+
+        private ValueTask<bool> CityExists(int id)
+        {
+            return _repository.ExistsAsync<City>(x => x.Id == id);
+        }
+    }
+}
